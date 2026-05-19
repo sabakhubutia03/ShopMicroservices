8175084b2513a046e7af7fdd06f9d27346c0762d
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-05-19 15:28:33 +0400

    feat: add User DTOs (Register, Login, UserResponse)

---
+++ UserService/User.Application/DTOs/UserResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace User.Application.DTOs;
+
+public class UserResponseDto
+{
+    public int Id { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public DateTime Created { get; set; }
+}
