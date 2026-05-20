685b72d4ea7f6ae4fc6d74358888197f3b3bc885
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-05-20 11:08:34 +0400

    feat(UserService): implement UserService with Register, Login and GetById

--- UserService/User.Application/Service/UserService.cs
+++ UserService/User.Application/Service/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using User.Application.DTOs;
 using User.Application.Interface;
+using User.Domein.Exceptions;
 
 namespace User.Application.Service;
 
@@ -12,18 +13,106 @@ public class UserService : IUserService
         _userRepository = userRepository;
     }
     
-    public Task<UserResponseDto> Register(RegisterDto dto)
+    public async Task<UserResponseDto> Register(RegisterDto dto)
     {
-        throw new NotImplementedException();
+        var checkEmail = await _userRepository.GetByEmail(dto.Email);
+        if (checkEmail != null)
+        {
+            throw new ApiException(
+                "Email already exists!",
+                "Conflict",
+                409,
+                "Email already exists!",
+                "Email already exists!"
+            );
+        } 
+        
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+        
+       var user = new Domain.Entity.User
+        {
+            Username = dto.UserName,
+            Email = dto.Email,
+            PasswordHash = passwordHash,
+            CreatedAt = DateTime.UtcNow
+        };
+       
+      var createUser = await _userRepository.Create(user);
+      if (createUser == null)
+      {
+          throw new ApiException(
+              "Failed to create user",
+              "Bad Request",
+              400,
+              "Failed to create user",
+              "USER_CREATE_FAILED"
+          );
+      }
+
+        return new UserResponseDto
+        {
+            Id = createUser.Id,
+            UserName = createUser.Username,
+            Email = createUser.Email,
+            Created = createUser.CreatedAt,
+        };
     }
 
-    public Task<UserResponseDto> Login(LoginDto dto)
+    public async Task<UserResponseDto> Login(LoginDto dto)
     {
-        throw new NotImplementedException();
+        var checkEmail = await _userRepository.GetByEmail(dto.Email);
+        if (checkEmail == null)
+        {
+            throw new ApiException(
+                "User not found",
+                "NotFound",
+                404,
+                "User not found",
+                "USER_LOGIN_FAILED"
+            );
+        }
+        
+        var checkPassword = BCrypt.Net.BCrypt.Verify(dto.Password, checkEmail.PasswordHash);
+        if (!checkPassword)
+        {
+            throw new ApiException(
+                "Invalid password or email.",
+                "Bad Request",
+                400,
+                "Invalid password or email.",
+                "USER_LOGIN_FAILED"
+            );
+        }
+
+        return new UserResponseDto
+        {
+            Id = checkEmail.Id,
+            UserName = checkEmail.Username,
+            Email = checkEmail.Email,
+            Created = checkEmail.CreatedAt,
+        };
     }
 
-    public Task<UserResponseDto> GetById(int id)
+    public async Task<UserResponseDto> GetById(int id)
     {
-        throw new NotImplementedException();
+        var getUser = await _userRepository.GetById(id);
+        if (getUser == null)
+        {
+            throw new ApiException(
+                $"User not found Id - {id}",
+                "NotFound",
+                400,
+                "User not found",
+                "USER_LOAD_FAILED"
+            );
+        }
+
+        return new UserResponseDto
+        {
+            Id = getUser.Id,
+            UserName = getUser.Username,
+            Email = getUser.Email,
+            Created = getUser.CreatedAt
+        };
     }
 }
