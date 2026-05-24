5be97c3278c17251aa529f62732df6a069066d66
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-05-24 14:18:36 +0400

    Add Swagger aggregator to ApiGateway

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,10 +1,20 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using MMLib.SwaggerForOcelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json",optional:false, reloadOnChange:true );
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddOcelot();
 var app = builder.Build();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+});
+
 await app.UseOcelot();
 app.Run();
