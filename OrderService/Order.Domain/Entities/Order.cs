120e52843e6450bf9dd0e2f0b6843917cd89916f
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-05-26 10:42:47 +0400

    Add Order entity, exceptions, middleware and project setup

---
+++ OrderService/Order.Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Order.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime Date { get; set; } = DateTime.UtcNow; 
+}
